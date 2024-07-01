6880052464e3f54e0f4e5bd0ebef5607fd9bb947
Stephen <stephen@behnfeldt.pro>
2024-07-01 12:52:35 -0400

    Begin converting Tailwind to SASS

--- resources/js/Pages/Classrooms.jsx
+++ resources/js/Pages/Classrooms.jsx
@@ -12,7 +12,7 @@ const Classrooms = ({auth}) => {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">Lorem ipsum dolor sit amet.</div>
+                        <div className="content">Lorem ipsum dolor sit amet.</div>
                     </div>
                 </div>
             </div>

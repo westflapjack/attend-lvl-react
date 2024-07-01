e0024b6749616f4ac4a7a81a634e6c8f01aff6a5
Stephen <stephen@behnfeldt.pro>
2024-07-01 12:45:43 -0400

    Fixed two small errors (typos) in the Classrooms page component.

--- resources/js/Pages/Classrooms.jsx
+++ resources/js/Pages/Classrooms.jsx
@@ -5,9 +5,9 @@ const Classrooms = ({auth}) => {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Classrooms</h2>}
         >
-            <Head title="Dashboard"/>
+            <Head title="Classrooms"/>
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">

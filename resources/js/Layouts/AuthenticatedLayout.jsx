6cfb32db7af364229ebdb5a3e6a469fa97a6ac80
Stephen <stephen@behnfeldt.pro>
2024-07-05 14:14:54 -0400

    Noted the different sections of the authenticated layout navbar

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -9,10 +9,14 @@ export default function Authenticated({user, header, children}) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
     return (
-        <div className="min-h-screen bg-gray-100">
-            <nav className="bg-white border-b border-gray-100">
+        <div className="authenticated-layout">
+
+            {/* Nav Bar: Top-level navigation bar */}
+            <nav className="navbar">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
+
+                        {/* Nav Links : Links to application-specific pages */}
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
@@ -30,6 +34,7 @@ export default function Authenticated({user, header, children}) {
                             </div>
                         </div>
 
+                        {/* Profile Menu */}
                         <div className="hidden sm:flex sm:items-center sm:ms-6">
                             <div className="ms-3 relative">
                                 <Dropdown>
@@ -67,7 +72,9 @@ export default function Authenticated({user, header, children}) {
                             </div>
                         </div>
 
+                        {/* Hamburger Menu */}
                         <div className="-me-2 flex items-center sm:hidden">
+
                             <button
                                 onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
                                 className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
@@ -93,6 +100,7 @@ export default function Authenticated({user, header, children}) {
                     </div>
                 </div>
 
+                {/* Profile Menu */}
                 <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
                     <div className="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>

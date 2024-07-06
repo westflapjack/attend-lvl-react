3befe24df7d80b6ada84cdfc29cb37e9682c77ef
Stephen <stephen@behnfeldt.pro>
2024-07-06 10:23:23 -0400

    Do some styling for <form> elements, and the Login form in particular

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -32,25 +32,28 @@ export default function Login({status, canResetPassword}) {
 
             {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
 
-            <form onSubmit={submit}>
+            <form className="login-form" onSubmit={submit}>
                 <div>
-                    <InputLabel htmlFor="email" value="Email"/>
+                    <div className="form-box">
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
+                        <InputLabel htmlFor="email" value="Email"/>
 
-                    <InputError message={errors.email} className="mt-2"/>
+                        <TextInput
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={data.email}
+                            className="mt-1 block w-full"
+                            autoComplete="username"
+                            isFocused={true}
+                            onChange={(e) => setData('email', e.target.value)}
+                        />
+
+                        <InputError message={errors.email} className="mt-2"/>
+                    </div>
                 </div>
 
-                <div className="mt-4">
+                <div className="form-box">
                     <InputLabel htmlFor="password" value="Password"/>
 
                     <TextInput
@@ -78,19 +81,19 @@ export default function Login({status, canResetPassword}) {
                 </div>
 
                 <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
                     <PrimaryButton className="ms-4" disabled={processing}>
                         Log in
                     </PrimaryButton>
                 </div>
+                <hr/>
+                {canResetPassword && (
+                    <Link
+                        href={route('password.request')}
+                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Forgot your password?
+                    </Link>
+                )}
             </form>
         </GuestLayout>
     );

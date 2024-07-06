8fd6eece4e87bc220d514c4ea36f05921575e4f3
Stephen <stephen@behnfeldt.pro>
2024-07-06 07:41:50 -0400

    Styled nav menu

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -5,21 +5,25 @@ export default function Authenticated({user, header, children}) {
 
 
     return (
+
         <div className="authenticated-layout">
-            <nav className="navbar">
-                <div className="nav-menu">
-                    <NavLink href={route('dashboard')} active={route().current('dashboard')}>
-                        Dashboard
-                    </NavLink>
-                    <NavLink href={route('classrooms')} active={route().current('classrooms')}>
-                        Classrooms
-                    </NavLink>
-                </div>
-            </nav>
+            <header>
+                <nav className="navbar">
+                    <div className="nav-menu">
+                        <NavLink href={route('dashboard')} active={route().current('dashboard')}>
+                            Dashboard
+                        </NavLink>
+                        <NavLink href={route('classrooms')} active={route().current('classrooms')}>
+                            Classrooms
+                        </NavLink>
+                    </div>
+                </nav>
+            </header>
 
-            <h2>{header}</h2>
-
-            <main>{children}</main>
+            <main>
+                <h2>{header}</h2>
+                {children}
+            </main>
         </div>
     );
 }
--- resources/js/Pages/Classrooms.jsx
+++ resources/js/Pages/Classrooms.jsx
@@ -7,17 +7,13 @@ const Classrooms = ({auth, classrooms}) => {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Classrooms</h2>}
+            header={<h2>Classrooms</h2>}
         >
             <Head title="Classrooms"/>
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="content">
-                            <ClassroomsTable classrooms={classrooms}/>
-                        </div>
-                    </div>
+                <div className="content">
+                    <ClassroomsTable classrooms={classrooms}/>
                 </div>
             </div>
         </AuthenticatedLayout>
